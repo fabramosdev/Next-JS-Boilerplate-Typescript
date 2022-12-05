e8e954170fd67405a74018ff8ea30bdf5193eaca
Fabano S. Ramos <fabiano.suporteinfo@gmail.com>
2022-12-05 09:06:09 -0300

    pwa added

--- next.config.js
+++ next.config.js
@@ -1,6 +1,20 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+/* eslint-disable @typescript-eslint/no-var-requires */
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: true,
+// }
 
-module.exports = nextConfig
+// module.exports = nextConfig
+
+const isProd = process.env.NODE_ENV === 'production'
+
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+  disable: !isProd,
+})
+
+module.exports = withPWA({
+  reactStrictMode: true,
+})
